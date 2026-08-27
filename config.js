022077b75434908bc96371e27af6c924a3a9e235
vikousek18 <159587706+vikousek18@users.noreply.github.com>
2026-08-27 03:51:03 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,5 @@
-/*
-  PUT YOUR GOOGLE APPS SCRIPT WEB APP URL HERE.
-  Example:
+
   const CONFIG = {
     APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwv1GZNro5kMUjsgtyzSd-Duuluo1mCpviq2z2Ji_KRhI8kP3p1l6ford3H8CtbohlhWg/exec"
   };
-*/
-const CONFIG = {
-  APPS_SCRIPT_URL: ""
-};
+
