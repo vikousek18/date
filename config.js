fd4352a23316a2e43f0209e400b2a6faf9147513
vikousek18 <159587706+vikousek18@users.noreply.github.com>
2026-08-27 08:24:40 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 
   const CONFIG = {
-    APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwv1GZNro5kMUjsgtyzSd-Duuluo1mCpviq2z2Ji_KRhI8kP3p1l6ford3H8CtbohlhWg/exec"
+    APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwN8j8v_Gg95zQ66Fd4CvbFrl1eYQoxQ1GMvKzyV9Jc6FSnupj7vU6fkCHFuORITZIyhw/exec"
   };
 
